71c48ec0785d15a15ca0f9b1f045bf246758cf77
Matheussansalone <sansalonematheus@gmail.com>
2024-12-04 09:11:32 -0300

    Burguer-Challenge

---
+++ scripts.js
@@ -0,0 +1,63 @@
+const list = document.querySelector('ul')
+const buttonShowAll = document.querySelector('.show-all')
+const buttonMapall = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filter-all')
+
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL',
+    })
+
+    return newValue
+}
+
+let myLi = ''
+
+function showAll(productsArray) {
+    myLi = ''
+    productsArray.forEach((product) => {
+        myLi +=
+            `
+                    <li>
+                        <img src=${product.src}>
+                        <p>${product.name}</p>
+                        <p class="item-price">R$ ${formatCurrency(product.price)}</p>
+                    </li>
+                `
+
+    })
+    list.innerHTML = myLi
+}
+function mapAllItems() {
+    const newPrice = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
+    }))
+
+    showAll(newPrice)
+}
+
+function sumAllItems() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+
+    list.innerHTML =
+        `
+            <li>
+                <p>O valor total dos intens é R$ ${formatCurrency(totalValue)}</p>
+            </li>
+        `
+}
+
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter((product) => product.vegan)
+
+    showAll(filterJustVegan)
+}
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapall.addEventListener('click', mapAllItems)
+sumAll.addEventListener('click', sumAllItems)
+filterAll.addEventListener('click', filterAllItems)
